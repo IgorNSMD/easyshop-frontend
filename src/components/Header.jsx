1105b4e096739ca3b4e8dcf1da40a05e8b552d3a
IgorNSMD <isanmartin131210@hotmail.cl>
2024-08-19 16:06:47 -0400

    cap243

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdEmail } from "react-icons/md";
 import { IoMdPhonePortrait } from "react-icons/io";
-import { FaFacebookF, FaLock, FaUser } from "react-icons/fa";
+import { FaFacebookF, FaList, FaLock, FaUser } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
  
 
 const Header = () => {
-
+    const {pathname} = useLocation()
+     
+    const [showShidebar, setShowShidebar] = useState(true);
     const user = true
 
     return (
@@ -39,26 +41,71 @@ const Header = () => {
                                     <a href="#"><FaLinkedin /></a>
                                     <a href="#"><FaGithub /> </a> 
                                 </div>
-                                <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]'>
-                                    <img src="http://localhost:3000/images/language.png" alt="" />
-                                    <span><IoMdArrowDropdown /></span>
-                                    <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
-                                    <li>Hindi</li>
-                                    <li>English</li>
-                                    </ul>
-                                </div>
-                                {
-                                    user ? <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/dashboard'>
-                                        <span> <FaUser/> </span>
-                                        <span>Kazi Ariyan </span>
-                                        </Link> : <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/login'>
-                                        <span> <FaLock /> </span>
-                                        <span>Login </span>
-                                        </Link>
-                                }
+        <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]'>
+            <img src="http://localhost:3000/images/language.png" alt="" />
+            <span><IoMdArrowDropdown /></span>
+            <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
+            <li>Hindi</li>
+            <li>English</li>
+            </ul>
+        </div>
+
+        {
+            user ? <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/dashboard'>
+                <span> <FaUser/> </span>
+                <span>Kazi Ariyan </span>
+                 </Link> : <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/login'>
+                <span> <FaLock /> </span>
+                <span>Login </span>
+                 </Link>
+        }
+ 
                             </div>
+                        </div> 
+                    </div> 
+                </div> 
+            </div>
+
+            <div className='w-white'>
+                <div className='w-[85%] lg:w-[90%] mx-auto'>
+                    <div className='h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
+                        <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
+                            <div className='flex justify-between items-center'>
+                                <Link to='/'>
+                                    <img src="http://localhost:3000/images/logo.png" alt="" />
+                                </Link>
+                                <div className='justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden' onClick={() => setShowShidebar(false)}>
+                                    <span> <FaList/> </span>
+                                </div>
+                            </div> 
                         </div>
-                    </div>
+        
+                        <div className='md:lg:w-full w-9/12'>
+                            <div className='flex justify-between md-lg:justify-center items-center flex-wrap pl-8'>
+                                <ul className='flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden'>
+                                    <li>
+                                        <Link className={`p-2 block ${pathname === '/' ?  'text-[#059473]' : 'text-slate-600' } `} >Home</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link className={`p-2 block ${pathname === '/shop' ?  'text-[#059473]' : 'text-slate-600' } `} >Shop</Link>
+                                    </li>
+                                    <li>
+                                        <Link className={`p-2 block ${pathname === '/blog' ?  'text-[#059473]' : 'text-slate-600' } `} >Blog</Link>
+                                    </li>
+                                    <li>
+                                        <Link className={`p-2 block ${pathname === '/about' ?  'text-[#059473]' : 'text-slate-600' } `} >About Us</Link>
+                                    </li>
+                                    <li>
+                                        <Link className={`p-2 block ${pathname === '/contact' ?  'text-[#059473]' : 'text-slate-600' } `} >Contact Us</Link>
+                                    </li>
+
+                                </ul>
+
+                            </div>
+
+                        </div>
+                    </div> 
                 </div>
             </div>
         </div>
