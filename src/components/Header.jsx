3093fa2cc1fd1c580f2151ec7e5f8eec23201c0a
IgorNSMD <isanmartin131210@hotmail.cl>
2024-08-19 17:33:11 -0400

    cap248

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,6 +19,19 @@ const Header = () => {
     const [categoryShow, setCategoryShow] = useState(true);
     const user = true
     const wishlist_count = 3
+    const categorys = [
+        'Mobiles',
+        'Laptops',
+        'Speakers',
+        'Top wear',
+        'Footwear',
+        'Watches',
+        'Home Decor',
+        'Smart Watches',
+    ]
+
+    const [searchValue, setSearchValue] = useState('')
+    const [category, setCategory] = useState('')
 
     return (
         <div className='w-full bg-white'>
@@ -75,7 +88,6 @@ const Header = () => {
                     <div className='h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
                         <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
                             <div className='flex justify-between items-center'>
-        
                                 <Link to='/'>
                                     <img src="http://localhost:3000/images/logo.png" alt="" />
                                 </Link>
@@ -107,27 +119,31 @@ const Header = () => {
 
                                 </ul>
 
-                                <div className='flex md-lg:hidden justify-center items-center gap-5'>
-                                    <div className='flex justify-center gap-5'>
-                                        <div className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
-                                            <span className='text-xl text-green-500'><FaHeart /></span>
-                                            <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] '>
-                                                {
-                                                    wishlist_count
-                                                }
-                                            </div>
-                                        </div>
+                            <div className='flex md-lg:hidden justify-center items-center gap-5'>
+                                <div className='flex justify-center gap-5'>
+                                    <div className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
+                                        <span className='text-xl text-green-500'><FaHeart /></span>
+                        <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] '>
+                            {
+                                wishlist_count
+                            }
 
-                                        <div className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
-                                            <span className='text-xl text-green-500'><FaCartShopping  /></span>
-                                            <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] '>
-                                                {
-                                                    wishlist_count
-                                                }
+                                            </div>
+                                    </div>
+
+                                    <div className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
+                                        <span className='text-xl text-green-500'><FaCartShopping  /></span>
+                        <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] '>
+                            {
+                                wishlist_count
+                            }
+
                                             </div> 
-                                        </div> 
                                     </div> 
                                 </div> 
+                            </div> 
+
+
                             </div> 
                         </div>
                     </div> 
@@ -145,12 +161,12 @@ const Header = () => {
                         </Link>
                         <div className='flex justify-start items-center gap-10'>
                             <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute '>
-                                <img src="http://localhost:3000/images/language.png" alt="" />
-                                <span><IoMdArrowDropdown /></span>
-                                <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
-                                <li>Hindi</li>
-                                <li>English</li>
-                                </ul>
+                                    <img src="http://localhost:3000/images/language.png" alt="" />
+                                    <span><IoMdArrowDropdown /></span>
+                                    <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
+                                    <li>Hindi</li>
+                                    <li>English</li>
+                                    </ul>
                             </div>
                             {
                                 user ? <Link className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black' to='/dashboard'>
@@ -161,6 +177,7 @@ const Header = () => {
                                     <span>Login </span>
                                     </Link>
                             } 
+
                         </div>
 
                         <ul className='flex flex-col justify-start items-start text-sm font-bold uppercase'>
@@ -213,20 +230,53 @@ const Header = () => {
                 <div className='flex w-full flex-wrap md-lg:gap-8'>
                     <div className='w-3/12 md-lg:w-full'>
                         <div className='bg-white relative'>
-                            <div onClick={() => setCategoryShow(!categoryShow) } className='h-[50px] bg-[#059473] text-white flex justify-center md-lg:justify-between md-lg:px-6 items-center gap-3 font-bold text-md cursor-pointer'>
-                                <div className='flex justify-center items-center gap-3'>
-                                    <span><FaList/></span>
-                                    <span>All Category </span>
-                                </div>
-                                <span className='pt-1'><IoIosArrowDown /></span>
+                        <div onClick={() => setCategoryShow(!categoryShow) } className='h-[50px] bg-[#059473] text-white flex justify-center md-lg:justify-between md-lg:px-6 items-center gap-3 font-bold text-md cursor-pointer'>
+                    <div className='flex justify-center items-center gap-3'>
+                        <span><FaList/></span>
+                        <span>All Category </span>
+                    </div>
+                    <span className='pt-1'><IoIosArrowDown /></span>
                             </div>
 
-                            <div className={`${categoryShow ? 'h-0' : 'h-[400px]'} overflow-hidden transition-all md-lg:relative duration-500 absolute z-[99999] bg-[#dbf3ed] w-full border-x`}>
+                <div className={`${categoryShow ? 'h-0' : 'h-[400px]'} overflow-hidden transition-all md-lg:relative duration-500 absolute z-[99999] bg-[#dbf3ed] w-full border-x`}>
+                    <ul className='py-2 text-slate-600 font-medium'>
+                        {
+                            categorys.map((c,i) => {
+                                return (
+                                <li key={i} className='flex justify-start items-center gap-2 px-[24px] py-[6px]'>
+                                    <Link className='text-sm block'>{c}</Link>
+                                </li>
+                                )
+                            })
+                        }
+                    </ul>
 
-                            </div>
+                </div>
+
+
                         </div>
                     </div>
-                </div>
+
+                    <div className='w-9/12 pl-8 md-lg:pl-0 md-lg:w-full'>
+                        <div className='flex flex-wrap w-full justify-between items-center md-lg:gap-6'>
+                            <div className='w-8/12 md-lg:w-full'>
+                                <div className='flex border h-[50px] items-center relative gap-6'>
+                                    <div className='relative after:absolute after:h-[25px] after:w-[1px] after:bg-[#afafaf] after:-right-[15px] md:hidden'>
+                                        <select onChange={(e) => setCategory(e.target.value)} className='w-[150px] text-slate-600 font-semibold bg-transparent px-2 h-full outline-0 border-none' name="" id="">
+                                            <option value="">Select Category</option>
+                                            {
+                                                categorys.map((c, i) => <option value={c}>
+                                                    {c}
+                                                </option> )
+                                            }
+                                        </select>
+                                    </div>
+                                    <input className='w-full relative bg-transparent text-slate-500 outline-0 px-3 h-full' onChange={(e)=> setSearchValue(e.target.value)} type="text" name='' id='' placeholder='What do you need' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                </div> 
             </div>
           
         </div>
