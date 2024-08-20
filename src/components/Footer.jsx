57a0e37aa2d57d317c79bcc776f97a9d31c34815
IgorNSMD <isanmartin131210@hotmail.cl>
2024-08-19 21:42:23 -0400

    cap259

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaFacebookF} from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -20,43 +24,44 @@ const Footer = () => {
                     <div className='flex justify-center sm:justify-start sm:mt-6 w-full'>
                         <div>
                             <h2 className='font-bold text-lg mb-2'>Usefull Links </h2>
-                            <div className='flex justify-between gap-[80px] lg:gap-[40px]'>
-                                <ul className='flex flex-col gap-2 text-slate-600 text-sm font-semibold'>
-                                    <li>
-                                        <Link>About Us </Link>
-                                    </li>
-                                    <li>
-                                        <Link>About Our Shop </Link>
-                                    </li>
-                                    <li>
-                                        <Link>Delivery Information </Link>
-                                    </li>
-                                    <li>
-                                        <Link>Privacy Policy </Link>
-                                    </li>
-                                    <li>
-                                        <Link>Blogs  </Link>
-                                    </li>
-                                </ul>
+                                <div className='flex justify-between gap-[80px] lg:gap-[40px]'>
+                                    <ul className='flex flex-col gap-2 text-slate-600 text-sm font-semibold'>
+                                        <li>
+                                            <Link>About Us </Link>
+                                        </li>
+                                        <li>
+                                            <Link>About Our Shop </Link>
+                                        </li>
+                                        <li>
+                                            <Link>Delivery Information </Link>
+                                        </li>
+                                        <li>
+                                            <Link>Privacy Policy </Link>
+                                        </li>
+                                        <li>
+                                            <Link>Blogs  </Link>
+                                        </li>
+                                    </ul>
 
-                                <ul className='flex flex-col gap-2 text-slate-600 text-sm font-semibold'>
-                                    <li>
-                                        <Link>Our Service </Link>
-                                    </li>
-                                    <li>
-                                        <Link>Company Profile</Link>
-                                    </li>
-                                    <li>
-                                        <Link>Delivery Information </Link>
-                                    </li>
-                                    <li>
-                                        <Link>Privacy Policy </Link>
-                                    </li>
-                                    <li>
-                                        <Link>Blogs  </Link>
-                                    </li>
-                                </ul>
-                            </div>
+                                    <ul className='flex flex-col gap-2 text-slate-600 text-sm font-semibold'>
+                                        <li>
+                                            <Link>Our Service </Link>
+                                        </li>
+                                        <li>
+                                            <Link>Company Profile</Link>
+                                        </li>
+                                        <li>
+                                            <Link>Delivery Information </Link>
+                                        </li>
+                                        <li>
+                                            <Link>Privacy Policy </Link>
+                                        </li>
+                                        <li>
+                                            <Link>Blogs  </Link>
+                                        </li>
+                                    </ul>
+
+                                </div>
                         </div> 
                     </div> 
                 </div>
@@ -67,12 +72,32 @@ const Footer = () => {
                         <span>Get Email updates about tour latest and shop specials offers</span>
                         <div className='h-[50px] w-full bg-white border relative'>
                             <input className='h-full bg-transparent w-full px-3 outline-0' type="text" placeholder='Enter Your Email' />
-                            <button className='h-full absolute right-0 bg-[#059473] text-white uppercase px-4 font-bold text-sm'>Subscribe</button>
+                            <button className='h-full absolute right-0 bg-[#059473] text-white uppercase px-4 font-bold text-sm'>Subscribe</button>  
                         </div> 
-                    </div> 
-                </div>
+                        <ul className='flex justify-start items-center gap-3'>
+                            <li>
+                                <a className='w-[38px] h-[38px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-white rounded-full' href="#"><FaFacebookF/> </a>
+                            </li>
 
+                            <li>
+                                <a className='w-[38px] h-[38px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-white rounded-full' href="#"><FaTwitter/> </a>
+                            </li>
+                            <li>
+                                <a className='w-[38px] h-[38px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-white rounded-full' href="#"><FaLinkedin/> </a>
+                            </li>
+                            <li>
+                                <a className='w-[38px] h-[38px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-white rounded-full' href="#"><FaGithub/> </a>
+                            </li>
+
+                        </ul>
+                    </div> 
+                </div> 
             </div>
+
+            <div className='w-[90%] flex flex-wrap justify-center items-center text-slate-600 mx-auto py-5 text-center'>
+                <span>Copiright @ 2024 All Rights Reserved </span>
+            </div>
+           
         </footer>
     );
 };
