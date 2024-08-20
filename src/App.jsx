b4c427aabc19613a0360170034814dbdfc7ec306
IgorNSMD <isanmartin131210@hotmail.cl>
2024-08-20 14:16:41 -0400

    cap260

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Home from './pages/Home';
+import Shops from './pages/Shops';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home /> } />
+        <Route path='/shops' element={<Shops/>} />
       </Routes>
     </BrowserRouter>
   );
