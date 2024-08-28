a407058adddc178195dec9aa611997076574193f
IgorNSMD <isanmartin131210@hotmail.cl>
2024-08-28 17:05:25 -0400

    cap340

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -6,7 +6,8 @@ import toast from 'react-hot-toast';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { get_card_products,delete_card_product,messageClear, quantity_inc } from '../store/reducers/cardReducer';
+import { get_card_products,delete_card_product,messageClear,
+        quantity_inc, quantity_dec } from '../store/reducers/cardReducer';
 
 const Card = () => {
 
@@ -44,6 +45,13 @@ const Card = () => {
         const temp = quantity + 1;
         if (temp <= stock) {
             dispatch(quantity_inc(card_id))
+        }
+    }
+
+    const dec = (quantity, card_id) => {
+        const temp = quantity - 1;
+        if (temp !== 0) {
+            dispatch(quantity_dec(card_id))
         }
     }
 
@@ -105,7 +113,7 @@ const Card = () => {
                                                             </div>
                                                             <div className='flex gap-2 flex-col'>
                                                                 <div className='flex bg-slate-200 h-[30px] justify-center items-center text-xl'>
-                                                                    <div className='px-3 cursor-pointer'>-</div> 
+                                                                    <div onClick={() => dec(pt.quantity, pt._id )} className='px-3 cursor-pointer'>-</div> 
                                                                     <div className='px-3'>{pt.quantity }</div> 
                                                                     <div onClick={() => inc(pt.quantity,pt.productInfo.stock, pt._id )} className='px-3 cursor-pointer'>+</div> 
                                                                 </div>
@@ -147,11 +155,11 @@ const Card = () => {
                                                                 </div>
                                                                 <div className='flex gap-2 flex-col'>
                                                                     <div className='flex bg-slate-200 h-[30px] justify-center items-center text-xl'>
-                                                                        <div className='px-3 cursor-pointer'>-</div> 
+                                                                        <div onClick={() => dec(p.quantity, p._id )} className='px-3 cursor-pointer'>-</div> 
                                                                         <div className='px-3'>{p.quantity}</div> 
                                                                         <div className='px-3 cursor-pointer'>+</div> 
                                                                     </div>
-                                                                    <button className='px-5 py-[3px] bg-red-500 text-white'>Delete</button>
+                                                                    <button  onClick={() => dispatch(delete_card_product(p._id)) } className='px-5 py-[3px] bg-red-500 text-white'>Delete</button>
                                                                 </div>
                                                             </div>
                                                         </div>)
