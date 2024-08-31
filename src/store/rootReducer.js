3cc6c0f8974e179243b59ffbb46351ccb0ed7da0
IgorNSMD <isanmartin131210@hotmail.cl>
2024-08-30 20:25:45 -0400

    cap359

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,11 +2,13 @@ import homeReducer from "./reducers/homeReducer";
 import cardReducer from "./reducers/cardReducer";
 import authReducer from "./reducers/authReducer";
 import orderReducer from "./reducers/orderReducer";
+import dashboardReducer from "./reducers/dashboardReducer";
 
 const rootReducer = {
     home: homeReducer,
     auth: authReducer,
     card: cardReducer,
-    order: orderReducer 
+    order: orderReducer,
+    dashboard: dashboardReducer 
 }
 export default rootReducer;
